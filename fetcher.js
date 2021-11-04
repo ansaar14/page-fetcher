23d8bef97b3146e7dd6656b9dfe1839bc88216ec
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-04 00:13:29 +0000

    page fetcher

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const fs = require("fs");
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  
+  }
+  fs.writeFile(`${path}`, body, function(error) {
+    if (error) {
+      console.log("error:", error);
+    } else {
+      console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    }
+  })
+})
